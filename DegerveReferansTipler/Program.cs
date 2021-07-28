8b40adbf179fdc431fd1a9a59f5d4ba37edc7b4f
Hidayet Akkas <hidayetakkas97@gmail.com>
2021-07-28 20:39:19 +0300

    day 4Revert "Revert "day 4""
    This reverts commit 2bac000de70219b6362aa152ae3814273d3d0bcf.

--- DegerveReferansTipler/Program.cs
+++ DegerveReferansTipler/Program.cs
@@ -13,11 +13,12 @@ namespace DegerveReferansTipler
             //sayı1 ???  30
 
 
-            int[] sayılar1 = new int[] { 10, 20, 30 };
-            int[] sayılar2 = new int[] { 100, 200, 300 };
-            sayılar1 = sayılar2;
-            sayılar2[0] = 999;
+            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
             //sayılar1[0]  ??  999
         }
     }
 }
+ 
